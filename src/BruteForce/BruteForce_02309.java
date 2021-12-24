5d55c3df1890b16e4c69a3335ee5b6dd1a9268d7
Tacksang Kwon <sange17@naver.com>
2021-12-24 21:22:50 +0900

    Add bruteforce_02309.class

--- src/BruteForce/BruteForce_02309.java
+++ src/BruteForce/BruteForce_02309.java
@@ -12,46 +12,33 @@ public class BruteForce_02309 {
 		StringBuilder sb = new StringBuilder();
 		
 		int array[] = new int[9];
-		int cloneArray[] = new int[9]; 
 		int sum = 0;
-		int tmp = 0;
-		int tmpj = 0;
-		int tmpk = 0;
+		int a = 0;
+		int b = 0;
+		int result = 0;
 		
 		for(int i = 0; i < 9; i++) {
 			array[i] = Integer.parseInt(br.readLine());
-			cloneArray[i] = Integer.parseInt(br.readLine());
 			sum += array[i];
 		}
-		
 		Arrays.sort(array);
-		Arrays.sort(cloneArray);
 		
-		for(int j = 0; j < 9; j++) {
-			array = cloneArray;
-			tmp = sum;
-			tmp -= array[j];
+		for(int j = 0; j < 8; j++) {			
+			result = sum;
+			result -= array[j];
 			for(int k = j+1; k < 9; k++) {
-				array = cloneArray;
-				tmp -= array[k];
-				if(tmp == 100) {
-					tmpk = k;
-					break;
-				}else {
-					tmp += array[k];
+				if(result - array[k] == 100) {
+					a = j;
+					b = k;
 				}
-			}
-			if(tmp == 100) {
-				tmpj = j;
-				break;
 			}
 		}
 		
-		for(int h = 0; h < 9; h++) {
-			if(h == tmpj || h == tmpk) {
+		for(int m = 0; m < 9; m++) {
+			if(m == a || m == b) {
 				continue;
 			}else {
-				sb.append(array[h]).append("\n");
+				sb.append(array[m]).append('\n');				
 			}
 		}
 		

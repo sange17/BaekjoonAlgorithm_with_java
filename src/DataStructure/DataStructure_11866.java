9ce6b8cf2b2bec22fbf92b9457e1c945aa4d4ef0
Tacksang Kwon <sange17@naver.com>
2022-01-13 02:17:53 +0900

    Add datastructure_01158.java Add datastructure_11866.java

--- src/DataStructure/DataStructure_11866.java
+++ src/DataStructure/DataStructure_11866.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class DataStructure_01158 {
+public class DataStructure_11866 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		

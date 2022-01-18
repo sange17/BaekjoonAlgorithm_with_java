406145ed90c73717d5d585d57e75ec6c879dfc93
Tacksang Kwon <sange17@naver.com>
2022-01-18 13:05:29 +0900

    Add sorting_01427.java

---
+++ src/Sorting/Sorting_01427.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sorting_01427 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String array[] = br.readLine().split("");
+		
+		Arrays.sort(array);
+		
+		for(int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+		}
+		
+		System.out.print(sb.reverse());
+		br.close();
+	}
+}

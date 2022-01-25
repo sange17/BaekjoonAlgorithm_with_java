61f2e17d78e58095a0df128b64e51c3f1974668f
Tacksang Kwon <sange17@naver.com>
2022-01-25 23:20:28 +0900

    Add mathematics_01676.java

--- src/Mathematics/Mathematics_01676.java
+++ src/Mathematics/Mathematics_01676.java
@@ -1,17 +1,27 @@
-package DynamicProgramming;
+package Mathematics;
 
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class DynamicProgramming_01463 {
+public class Mathematics_01676 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int n = Integer.parseInt(br.readLine());
-		int count = 0;
 		
+		int fac = 1;
 		
-		System.out.println(count);
-		br.close();
+		while(true) {
+			if(n == 0) break;
+			
+			fac *= n;
+			
+			String str = Integer.toString(fac);
+			
+			n--;
+		}
+		
+		System.out.println(fac);
 	}
 }

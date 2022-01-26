5f7ae4cbcd4d0594e05c4a980b49bbbedcab6a0b
Tacksang Kwon <sange17@naver.com>
2022-01-26 19:49:00 +0900

    Add mathematics_02292.java

--- src/Mathematics/Mathematics_02292.java
+++ src/Mathematics/Mathematics_02292.java
@@ -8,8 +8,27 @@ public class Mathematics_02292 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int x = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int bfFloor = 1;
+		int afFloor = 1;
+		int count = 1;
+		int i = 1;
 		
-		
+		while(true) {
+			if(n == 1) break;
+			
+			bfFloor = afFloor;
+			afFloor += 6 * i;
+						
+			if(n > bfFloor && n <= afFloor) {
+				count++;
+				break;
+			}
+			
+			count++;
+			i++;
+		}
+		System.out.println(count);
+		br.close();
 	}
 }

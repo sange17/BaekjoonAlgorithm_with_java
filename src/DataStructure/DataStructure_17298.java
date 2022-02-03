2c60987d76b625479ae24eae75345b101c46667c
Tacksang Kwon <sange17@naver.com>
2022-02-03 19:56:12 +0900

    Add datastructure_17298.java

--- src/DataStructure/DataStructure_17298.java
+++ src/DataStructure/DataStructure_17298.java
@@ -12,27 +12,32 @@ public class DataStructure_17298 {
 				
 		int n = Integer.parseInt(br.readLine());
 		int array[] = new int[n];
+		int stack[] = new int[n];
+		int top = -1;
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
 		for(int i = 0; i < n; i++) {
 			array[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		for(int i = 0; i < n; i++) {
-			for(int j = i; j < n; j++) {
-				if(array[i] < array[j]) {
-					sb.append(array[j]).append(" ");
-					break;
-				}
-				
-				if(j == n - 1) {
-					sb.append(-1).append(" ");
-				}
+			while(top != -1 && array[stack[top]] < array[i]) {
+				array[stack[top]] = array[i];
+				top--;
 			}
+			top++;
+			stack[top] = i;
 		}
 		
-		System.out.print(sb);
+		for(int i = top; i >= 0; i--) {
+			array[stack[i]] = -1;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			sb.append(array[i]).append(" ");
+		}
+		
+		System.out.println(sb);
 		br.close();
 	}
 }

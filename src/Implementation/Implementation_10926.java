30412da58d39492fe15614a6b7a021d39ff9ab8f
Tacksang Kwon <sange17@naver.com>
2022-02-04 23:06:58 +0900

    Add implementation_10926.java

---
+++ src/Implementation/Implementation_10926.java
@@ -0,0 +1,15 @@
+package Implementation;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Implementation_10926 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		System.out.print(str + "??!");
+	}
+}
--- src/Sorting/Sorting_10814.java
+++ src/Sorting/Sorting_10814.java
@@ -3,47 +3,43 @@ package Sorting;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Sorting_10814 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
-		int max;
-		int numTemp; 
-		String nameTemp;
-		
 		int numArray[] = new int[n];
-		String nameArray[] = new String[n];
+		String strArray[] = new String[n];
 		
 		for(int i = 0; i < n; i++) {
-			String str[] = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine(), " ");
 			
-			numArray[i] = Integer.parseInt(str[0]);
-			nameArray[i] = str[1];
+			numArray[i] = Integer.parseInt(st.nextToken());
+			strArray[i] = st.nextToken();
 		}
-
-	    for (int i = numArray.length - 1; i >= 1 ; i--) {
-	        max = i;
-	        for (int j = numArray.length - 2; j >= 0; j--) {
-	            if (numArray[j] > numArray[max]) {
-	                max = j;
-	            }
-	        }
-	        numTemp = numArray[max];
-	        numArray[max] = numArray[i];
-	        numArray[i] = numTemp;
-	        nameTemp = nameArray[max];
-	        nameArray[max] = nameArray[i];
-	        nameArray[i] = nameTemp;
-	    }
-	    
-	    for(int i = 0; i < n; i++) {
-	    	sb.append(numArray[i]).append(" ").append(nameArray[i]).append("\n");
-	    }
-	    
-	    System.out.println(sb);
-	    br.close();
+		
+		int numTemp = 0;
+		String strTemp = "";
+		for(int i = 0; i < n; i++) {
+			for(int j = n - 1; j > i; j--) {
+				if(numArray[j] < numArray[j - 1]) {
+					numTemp = numArray[j-1];
+					numArray[j-1] = numArray[j];
+					numArray[j] = numTemp;
+					
+					strTemp = strArray[j-1];
+					strArray[j-1] = strArray[j];
+					strArray[j] = strTemp;
+				}				
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(numArray[i] + " " + strArray[i]);
+		}
 	}
 }

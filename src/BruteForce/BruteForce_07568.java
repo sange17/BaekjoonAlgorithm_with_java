46600784c271274f98a8c8d47737ac27b66443aa
Tacksang Kwon <sange17@naver.com>
2022-02-09 10:25:06 +0900

    Add bruteforce_07568.java

--- src/BruteForce/BruteForce_07568.java
+++ src/BruteForce/BruteForce_07568.java
@@ -3,6 +3,7 @@ package BruteForce;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BruteForce_07568 {
 	public static void main(String[] args) throws IOException{
@@ -10,12 +11,29 @@ public class BruteForce_07568 {
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
+		int array[][] = new int[n][2];
+		
+		StringTokenizer st;
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int weight = Integer.parseInt(st.nextToken());
+			int length = Integer.parseInt(st.nextToken());
+			
+			array[i][0] = weight;
+			array[i][1] = length;
+		}
 		
 		for(int i = 0; i < n; i++) {
-			String array[] = br.readLine().split(" ");
-			int size = Integer.parseInt(array[0]) * Integer.parseInt(array[1]);
-			
-			
+			int rank = 1;
+			for(int j = 0; j < n; j++) {
+				if(array[i][0] < array[j][0] && array[i][1] < array[j][1]) {
+					rank++;
+				}
+			}
+			sb.append(rank).append(" ");
 		}
+		System.out.print(sb);
+		br.close();
 	}
 }

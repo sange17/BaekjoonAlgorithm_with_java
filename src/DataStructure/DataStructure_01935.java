4b50c1eccbfdbcc45b5a304aa3215236f8c710b5
Tacksang Kwon <sange17@naver.com>
2022-02-12 20:13:38 +0900

    Add datastructure_01935.java

--- src/DataStructure/DataStructure_01935.java
+++ src/DataStructure/DataStructure_01935.java
@@ -10,15 +10,60 @@ public class DataStructure_01935 {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		String str = br.readLine();
-		int array[] = new int[n];
+		String array[] = br.readLine().split("");
 		
-		for(int i = 65; i < 65 + n; i++) {
-			array[i] = Integer.parseInt(br.readLine());
+		int numArray[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			int a = Integer.parseInt(br.readLine());
+			numArray[i] = a;
 		}
 		
-		for(int i = 0; i < str.length(); i++) {
+		int idx = 0;
+		for(int i = 65; i < 65 + n; i++) {
+			for(int j = 0; j < array.length; j++) {
+				if(i == array[j].charAt(0)) {
+					array[j] = Integer.toString(numArray[idx]);
+				}
+			}
+			idx++;
+		}
+		
+		double stack[] = new double[array.length];
+		int size = 0;
+		double result = 0;
+		
+		for(int i = 0; i < array.length; i++) {
+			String ch = array[i];
+			switch(ch) {
+			case "*":
+				result = (double)stack[size-2] * (double)stack[size-1];
+				stack[size-2] = result;
+				size -= 1;
+				break;
+			case "/":
+				result = (double)stack[size-2] / (double)stack[size-1];
+				stack[size-2] = result;
+				size -= 1;
+				break;
+			case "+":
+				result = (double)stack[size-2] + (double)stack[size-1];
+				stack[size-2] = result;
+				size -= 1;
+				break;
+			case "-":
+				result = (double)stack[size-2] - (double)stack[size-1];
+				stack[size-2] = result;
+				size -= 1;
+				break;
+			default:
+				stack[size] = Integer.parseInt(ch);
+				size++;
+				break;
+			}
 			
 		}
+		System.out.printf("%.2f", stack[0]);
+		System.out.println();
+		br.close();
 	}
 }

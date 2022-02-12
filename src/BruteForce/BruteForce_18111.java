79e57b374d7fb28ce1f5f1e9d97cf0ac17486c7e
Tacksang Kwon <sange17@naver.com>
2022-02-12 17:41:56 +0900

    Update bruteforce_18111.java

--- src/BruteForce/BruteForce_18111.java
+++ src/BruteForce/BruteForce_18111.java
@@ -8,41 +8,62 @@ import java.util.StringTokenizer;
 public class BruteForce_18111 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int row = Integer.parseInt(st.nextToken());
 		int column = Integer.parseInt(st.nextToken());
-		int height = 256;
-		int in = 2;
-		int out = 1;
 		int inventory = Integer.parseInt(st.nextToken());
+		
 		int array[][] = new int[row][column];
-		int min = 99999999;
-		int max = 0;
+		int tmpArray[][] = new int[row][column];
+		int blockSum = 0;
+		int area = row * column;
+		int min = Integer.MAX_VALUE;
 		
 		for(int i = 0; i < row; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j = 0; j < column; j++) {
 				array[i][j] = Integer.parseInt(st.nextToken());
-				if(max < array[i][j]) {
-					max = array[i][j];
-				}
-				if(min > array[i][j]) {
-					min = array[i][j];
-				}
+				blockSum += array[i][j];
 			}
 		}
 		
-		for(int i = 0; i < row; i++) {
-			for(int j = 0; j < column; j++) {
-				array[i][j] = max - array[i][j];
-				sb.append(array[i][j]).append(" ");
+		int allBlockSum = inventory + blockSum;
+		int maxFloor = allBlockSum / area;
+		
+		int timeArray[] = new int[maxFloor+1];
+		int digTime = 0;
+		int fillTime = 0;
+		
+		int f = 0;
+		while(maxFloor >= 0) {
+			for(int i = 0; i < row; i++) {
+				for(int j = 0; j < column; j++) {
+					tmpArray[i][j] = array[i][j] - maxFloor;
+					if(tmpArray[i][j] > 0) {
+						digTime += (tmpArray[i][j] * 2);
+					}else if(tmpArray[i][j] < 0) {
+						fillTime += (tmpArray[i][j] * -1);
+					}
+				}
 			}
-			sb.append("\n");
+
+			timeArray[f] = digTime + fillTime;
+			digTime = 0;
+			fillTime = 0;
+			f++;
+			maxFloor--;
 		}
 		
-		System.out.println(sb);
+		int height = 0;
+		for(int i = 0; i < allBlockSum / area + 1; i++) {
+			if(timeArray[i] < min) {
+				min = timeArray[i];
+				height = allBlockSum / area - i;
+			}
+		}
+		
+		System.out.println(min + " " + height);
 		br.close();
 	}
 }

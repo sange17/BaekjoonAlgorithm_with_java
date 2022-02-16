3a5d73bd359749344a1bfbf404046f46cb7df855
Tacksang Kwon <sange17@naver.com>
2022-02-16 16:51:07 +0900

    Add dynamicprogramming_01003.java

--- src/DynamicProgramming/DynamicProgramming_01003.java
+++ src/DynamicProgramming/DynamicProgramming_01003.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DynamicProgramming_01003 {
-	public static int count0 = 0;
-	public static int count1 = 0;
+	static int[] check = new int[41];
 	
 	public static int fib(int a) {
 		if(a == 0) {
-			count0++;
 			return 0;
 		}else if(a == 1) {
-			count1++;
 			return 1;
+		}else if(check[a] != 0) {
+			return check[a];
+		}else {
+			return check[a] = (fib(a-1) + fib(a-2));			
 		}
-		return (fib(a-1) + fib(a-2));
 	}
 	
 	public static void main(String[] args) throws IOException{
@@ -24,14 +24,21 @@ public class DynamicProgramming_01003 {
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
+		int[][] dp = new int[41][2];
+		dp[0][1] = dp[1][0] = 0;
+		dp[0][0] = dp[1][1] = 1;
 		
 		for(int i = 0; i < n; i++) {
 			int num = Integer.parseInt(br.readLine());
 			
 			fib(num);
 			
-			sb.append(count0).append(" ").append(count1).append("\n");
-			count0 = count1 = 0;
+			for(int j = 2; j <= num; j++) {
+				dp[j][0] = dp[j-1][0] + dp[j-2][0];
+				dp[j][1] = dp[j-1][1] + dp[j-2][1];
+			}
+			
+			sb.append(dp[num][0]).append(" ").append(dp[num][1]).append("\n");
 		}
 		System.out.println(sb);
 		br.close();

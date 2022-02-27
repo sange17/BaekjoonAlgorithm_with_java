d4b1b3450bc5b51dc9609941f52ccdd759c9ce94
Tacksang Kwon <sange17@naver.com>
2022-02-27 12:23:33 +0900

    Add dynamicprogramming_16194.java

---
+++ src/DynamicProgramming/DynamicProgramming_16194.java
@@ -0,0 +1,31 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class DynamicProgramming_16194 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] array = new int[N + 1];
+		int[] dpArray = new int[N + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 1; i < N + 1; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+			dpArray[i] = array[i];
+		}
+		
+		for(int i = 2; i < N + 1; i++) {
+			for(int j = 1; j <= i / 2; j++) {
+				dpArray[i] = Math.min(dpArray[i], dpArray[j] + dpArray[i - j]);
+			}
+		}
+		System.out.println(dpArray[N]);
+		br.close();
+	}
+}

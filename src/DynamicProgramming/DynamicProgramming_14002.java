9ac86af7e6520f6ec47c504ccb52a29c1c395f02
Tacksang Kwon <sange17@naver.com>
2022-03-01 12:20:12 +0900

    Add dynamicprogramming_14002.java

---
+++ src/DynamicProgramming/DynamicProgramming_14002.java
@@ -0,0 +1,55 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class DynamicProgramming_14002 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] array = new int[n + 1];
+		int[] dpArray = new int[n + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");	
+		for(int i = 1; i <= n; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dpArray[1] = 1;
+		for(int i = 2; i <= n; i++) {
+			dpArray[i] = 1;
+			for(int j = 1; j < i; j++) {
+				if(array[i] > array[j]) {
+					dpArray[i] = Math.max(dpArray[j] + 1, dpArray[i]);
+				}
+			}
+		}
+		
+		int max = 0;
+		for(int i = 1; i <= n; i++) {
+			if(max < dpArray[i]) {
+				max = dpArray[i];
+			}
+		}
+		
+		sb.append(max).append("\n");
+		
+		int[] result = new int[max + 1];
+		for(int i = n; i >= 1; i--) {
+			if(max == dpArray[i]) {
+				result[max--] = array[i];
+			}
+		}
+		
+		for(int i = 1; i <= result.length - 1; i++) {
+			sb.append(result[i]).append(" ");
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}

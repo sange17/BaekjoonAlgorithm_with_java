6d1537e167a145bd7c6cd0270fbab59f8bd088f2
Tacksang Kwon <sange17@naver.com>
2022-03-06 17:53:23 +0900

    Add implementation_05337.java

---
+++ src/Implementation/Implementation_05337.java
@@ -0,0 +1,9 @@
+package Implementation;
+
+public class Implementation_05337 {
+	public static void main(String[] args){
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}

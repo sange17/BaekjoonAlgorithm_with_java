0c58a83e8d54be92e524ebd695135acddf31916a
Tacksang Kwon <sange17@naver.com>
2022-03-06 17:54:02 +0900

    Add implementation_05338.java

---
+++ src/Implementation/Implementation_05338.java
@@ -0,0 +1,11 @@
+package Implementation;
+
+public class Implementation_05338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}

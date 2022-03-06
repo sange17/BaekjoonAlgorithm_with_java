78ada595595c433c117dd9f946ad247343d84cb5
Tacksang Kwon <sange17@naver.com>
2022-03-06 17:59:04 +0900

    Add mathematics_05522.java

---
+++ src/Mathematics/Mathematics_05522.java
@@ -0,0 +1,19 @@
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Mathematics_05522 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0;
+		for(int i = 0; i < 5; i++) {
+			sum += Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum);
+		br.close();
+	}
+}

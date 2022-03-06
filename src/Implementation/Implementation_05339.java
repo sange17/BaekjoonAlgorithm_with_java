e1195b6ca9265e524b23884c3add744f59bc90e6
Tacksang Kwon <sange17@naver.com>
2022-03-06 17:54:57 +0900

    Add implementation.java

---
+++ src/Implementation/Implementation_05339.java
@@ -0,0 +1,16 @@
+package Implementation;
+
+public class Implementation_05339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}

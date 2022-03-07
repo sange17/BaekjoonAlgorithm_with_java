0eb2fa264b55b57d1f37f365401875af823b54ba
Tacksang Kwon <sange17@naver.com>
2022-03-07 13:53:42 +0900

    Add dynamicprogramming_09660.java

---
+++ src/DynamicProgramming/DynamicProgramming_09660.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DynamicProgramming_09660 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long n = Long.parseLong(br.readLine());
+		
+		if(n % 7 == 0 || n % 7 == 2) {
+			System.out.println("CY");
+		}else {
+			System.out.println("SK");
+		}
+		br.close();
+	}
+}

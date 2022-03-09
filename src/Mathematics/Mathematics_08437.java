c2899a539da568f799f6463377a028201047c3b8
Tacksang Kwon <sange17@naver.com>
2022-03-09 09:50:45 +0900

    Add mathematics_08437.java

--- src/Mathematics/Mathematics_08437.java
+++ src/Mathematics/Mathematics_08437.java
@@ -11,8 +11,17 @@ public class Mathematics_08437 {
 		
 		BigInteger ALL = new BigInteger(br.readLine());
 		BigInteger MORE = new BigInteger(br.readLine());
-		BigInteger SUM = ALL.add(MORE);
-		BigInteger DIV = new BigInteger("2");
+		BigInteger CHECK = new BigInteger("2");
+		BigInteger ODD = new BigInteger("1");
+		BigInteger EVEN = new BigInteger("0");
+		
+		if(ODD.compareTo(ALL.remainder(CHECK)) == 0 && ODD.compareTo(MORE.remainder(CHECK)) == 0) {
+			System.out.println(ALL.divide(CHECK).add(MORE.divide(CHECK).add(ODD)));
+			System.out.println(ALL.divide(CHECK).subtract(MORE.divide(CHECK)));
+		}else if(EVEN.compareTo(ALL.remainder(CHECK)) == 0 && EVEN.compareTo(MORE.remainder(CHECK)) == 0) {
+			System.out.println(ALL.divide(CHECK).add(MORE.divide(CHECK)));
+			System.out.println(ALL.divide(CHECK).subtract(MORE.divide(CHECK)));
+		}
 		
 		br.close();
 	}

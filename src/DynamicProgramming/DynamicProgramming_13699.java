675a155129f8fca2efd4be2ddc6ccd1c31ad3123
Tacksang Kwon <sange17@naver.com>
2022-03-12 13:46:44 +0900

    Add dynamicprogramming_13699.java

--- src/DynamicProgramming/DynamicProgramming_13699.java
+++ src/DynamicProgramming/DynamicProgramming_13699.java
@@ -9,14 +9,15 @@ public class DynamicProgramming_13699 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		int[] array = new int[35];
+		long[] array = new long[36];
 		
 		array[0] = 1;
 		array[1] = 1;
-		array[2] = 2;
 		
-		for(int i = 3; i <= n; i++) {
-			array[i] = array[i-1] + array[i-2];
+		for(int i = 2; i < array.length; i++) {
+			for(int j = 0; j < i; j++) {
+				array[i] += (array[j] * array[i-j-1]);				
+			}
 		}
 		
 		System.out.println(array[n]);

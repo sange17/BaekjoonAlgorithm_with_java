fe3709ed422e94154ee3850f58c62d47a14f4896
Tacksang Kwon <sange17@naver.com>
2022-03-13 23:33:31 +0900

    Add dynamicprogramming_01965.java

--- src/DynamicProgramming/DynamicProgramming_01965.java
+++ src/DynamicProgramming/DynamicProgramming_01965.java
@@ -13,25 +13,22 @@ public class DynamicProgramming_01965 {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int[] array = new int[n];
-		int[] dpArray = new int[n];
+		int[][] dpArray = new int[2][1001];
 		
-		for(int i = 0; i < n; i++) {
-			array[i] = Integer.parseInt(st.nextToken());
+		for(int i = 1; i <= n; i++) {
+			dpArray[0][i] = Integer.parseInt(st.nextToken());
 		}
 		
-		dpArray[0] = 1;
-		int max = 0;
-		for(int i = 1; i < n; i++) {
-			if(array[i-1] < array[i]) {
-				dpArray[i] = dpArray[i-1] + 1;
-			}else {
-				dpArray[i] = dpArray[i-1];
+		int max = Integer.MIN_VALUE;
+		for(int i = 1; i <= n; i++) {
+			dpArray[1][i] = 1;
+			for(int j = 1; j < i; j++) {
+				if(dpArray[0][j] < dpArray[0][i] && dpArray[1][i] < dpArray[1][j] + 1) {
+					dpArray[1][i] = dpArray[1][j] + 1;
+				}
 			}
-			
-			max = Math.max(max, dpArray[i]);
+			max = Math.max(max, dpArray[1][i]);
 		}
-		
 		System.out.println(max);
 		br.close();
 	}

6e137dc01a61f93e8938e62976f66ae68b82f8b5
Tacksang Kwon <sange17@naver.com>
2022-04-25 10:24:48 +0900

    Add bruteforce_15652.java

--- src/BruteForce/BruteForce_15652.java
+++ src/BruteForce/BruteForce_15652.java
@@ -1,30 +1,31 @@
 package BruteForce;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
-public class BruteForce_15651 {
+public class BruteForce_15652 {
 	static int N, M;
 	static int[] array;
 	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
 		array = new int[M];
 		
-		dfs(0);
+		dfs(0,0);
 		System.out.println(sb);
 		br.close();
 	}
 	
-	public static void dfs(int depth) {
+	public static void dfs(int depth, int start) {
 		if(depth == M) {
 			for(int i = 0; i < M; i++) {
 				sb.append(array[i]).append(" ");
@@ -33,9 +34,9 @@ public class BruteForce_15651 {
 			return;
 		}
 		
-		for(int i = 0; i < N; i++) {
+		for(int i = start; i < N; i++) {
 			array[depth] = i + 1;
-			dfs(depth + 1);
+			dfs(depth + 1, i);
 		}
 	}
 }

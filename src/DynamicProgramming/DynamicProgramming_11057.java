8b296d5616084d27c6e578aeffa3f98fd953b670
Tacksang Kwon <sange17@naver.com>
2022-05-21 10:15:06 +0900

    Add dynamicprogramming_11057.java

--- src/DynamicProgramming/DynamicProgramming_11057.java
+++ src/DynamicProgramming/DynamicProgramming_11057.java
@@ -1,13 +1,29 @@
 package DynamicProgramming;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class DynamicProgramming_11057 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		
+		int[][] dpArray = new int[N+1][10];
 		
+		for(int i = 0; i < 10; i++) dpArray[0][i] = 1;
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < 10; j++) {
+				for(int k = j; k < 10; k++) {
+					dpArray[i][j] += dpArray[i-1][k];					
+				}
+				dpArray[i][j] %= 10007;
+			}
+		}
+		
+		System.out.println(dpArray[N][0] % 10007);
+		br.close();
 	}
 }

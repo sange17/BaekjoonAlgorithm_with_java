1db655d72fd47db3444707f9c074a558c0e9a210
Tacksang Kwon <sange17@naver.com>
2022-06-12 20:02:18 +0900

    Add geometry_16483.java

---
+++ src/Geometry/Geometry_16483.java
@@ -0,0 +1,13 @@
+package Geometry;
+
+import java.util.Scanner;
+
+public class Geometry_16483 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double T = sc.nextInt();
+		
+		System.out.println(Math.round((T/2)*(T/2)));
+	}
+}

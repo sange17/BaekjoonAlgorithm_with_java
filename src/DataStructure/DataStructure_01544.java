c5383e30d4a70ac9a657164778f079fc2e49845c
Tacksang Kwon <sange17@naver.com>
2022-07-11 09:31:17 +0900

    Add datastructure_01544.java

---
+++ src/DataStructure/DataStructure_01544.java
@@ -0,0 +1,48 @@
+package DataStructure;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class DataStructure_01544 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		ArrayList<String>[] wordList = new ArrayList[N];
+		
+		int resultWordCount = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			wordList[i] = new ArrayList<>();
+			
+			String word = br.readLine();
+			
+			for(int j = 0; j < word.length(); j++) {
+				wordList[i].add(word.substring(j) + word.substring(0, j));
+			}
+			
+			
+			System.out.println(wordList[i]);
+			
+			boolean check = true;
+			for(int j = 0; check && j <= i - 1; j++) {
+				for(int k = 0; check && k < wordList[j].size(); k++) {
+					if(word.equals(wordList[j].get(k))) 
+						check = false;
+				}
+			}
+			if(check)
+				resultWordCount++;
+				
+		}
+		
+		System.out.println(resultWordCount);
+		br.close();
+		
+		
+	}
+}

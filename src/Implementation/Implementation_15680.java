b44003b1d7c15dfb4691a51a3005b4be629ffdaa
Tacksang Kwon <sange17@naver.com>
2022-08-12 17:08:24 +0900

    Add implementation_15680.java

---
+++ src/Implementation/Implementation_15680.java
@@ -0,0 +1,16 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class Implementation_15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N == 0) System.out.println("YONSEI");
+		else System.out.println("Leading the Way to the Future");
+		
+		sc.close();
+	}
+}

ab5b9923920a817996c42805c348c36cd827436e
Tacksang Kwon <sange17@naver.com>
2022-10-19 10:28:43 +0900

    Add datastructure_02493.java

--- src/DataStructure/DataStructure_02493.java
+++ src/DataStructure/DataStructure_02493.java
@@ -13,30 +13,33 @@ public class DataStructure_02493 {
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stackA = new Stack<Integer>();
-		Stack<Integer> stackB = new Stack<Integer>();
+		Stack<int[]> stack = new Stack<int[]>();
 		
 		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < N; i++) {
-			stackA.push(Integer.parseInt(st.nextToken()));
-		}
-		
-		while(!stackA.isEmpty()) {
-			int currentNum = stackA.pop();
+		for(int i = 1; i <= N; i++) {
+			int currentTop = Integer.parseInt(st.nextToken());
 			
-			if(stackA.peek() > currentNum) {
-				sb.append(stackA.size());
+			// 먼저 현재 탑의 높이와 이전에 스택에 들어가 있던 탑의 높이를 비교하고 후에 스택에 삽입한다.
+			while(!stack.isEmpty()) {
 				
-			}else { 
-				stackB.push(stackA.pop());
+				// 이전에 스택에 삽입한 배열 객체의 높이보다 스택에 있던 top의 높이가 높으면
+				if(stack.peek()[1] > currentTop) {
+					sb.append(stack.peek()[0] + " ");	// 이전에 삽입한 인덱스 번호 추가
+					break;
+				}
+				// 이전에 스택에 삽입한 배열 객체의 높이보다 스택에 있던 top의 높이가 낮으면
+				stack.pop();
 			}
 			
-			while(!stackB.isEmpty()) {
-				stackA.push(stackB.pop());
-				
+			if(stack.isEmpty()) {
+				sb.append(0 + " ");
 			}
+			
+			// 위의 과정을 마치고 스택에 인덱스와, 현재 타워의 높이를 배열 객체로 push한다.
+			stack.push(new int[] {i, currentTop});
 		}
 		
-		System.out.println(sb.reverse());
+		System.out.println(sb.toString());
+		br.close();
 	}
 }

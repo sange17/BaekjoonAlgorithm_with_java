e6cf82a6032b7881fe7e76b4253e2bd3800dfe88
Tacksang Kwon <sange17@naver.com>
2022-10-26 13:39:47 +0900

    Add datastructure_01406.java

--- src/DataStructure/DataStructure_01406.java
+++ src/DataStructure/DataStructure_01406.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
+// 문자열 길이 최대 십만개, 명령어 개수 오십만개
+// LinkedList는 삭제할 노드가 정해져 있다고 전제하에 O(1)이고, 그렇지 않다면 모두 탐색해야되는 O(n)을 갖는다.
 public class DataStructure_01406 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +15,9 @@ public class DataStructure_01406 {
 		
 		String str = br.readLine();
 		
-		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());	// 명령어 개수
+		
+		// list.size -> O(1)..?
 		LinkedList<Character> list = new LinkedList<Character>();
 		
 		for(int i = 0; i < str.length(); i++) {
@@ -23,90 +28,63 @@ public class DataStructure_01406 {
 		char operator;
 		char value;
 		int cursor = list.size();
-		for(int i = 0; i < N; i++) {
+		int strSize = list.size();
+		
+		// 모두 탐색하는 대신 커서의 위치에서 양방향으로 움직이며 삽입/삭제를 할 수 있다.
+		ListIterator<Character> iterator = list.listIterator();
+		
+		// 커서를 문자열 맨 뒤로 이동
+		while(iterator.hasNext()) {
+			iterator.next();
+		}
+		
+		for(int i = 0; i < M; i++) {
 			command = br.readLine();
 			operator = command.charAt(0);
 			
 			// 왼쪽으로 커서 이동
 			if(operator == 'L') {
-				cursor--;
 				
-				// 커서가 맨 왼쪽에 있을 때
-				if(cursor <= -1) {
-					cursor = -1;
+				if(iterator.hasPrevious()) {
+					iterator.previous();
 				}
+				continue;
 			}
 			
 			// 오른쪽으로 커서 이동
 			if(operator == 'D') {
-				cursor++;
-				
-				if(cursor == list.size()) {
-					cursor = list.size();
+								
+				if(iterator.hasNext()) {
+					iterator.next();
 				}
+				continue;
 			}
 
 			// 커서 왼쪽에 있는 값 삭제
 			if(operator == 'B') {
 				
-				// 커서가 중간에 있을 때
-				if(cursor < list.size() && cursor > -1) {
-					cursor--;
-					
-					// 커서가 중간에 있다가 맨 왼쪽으로 가게 되었을 때
-					if(cursor == -1) {
-						cursor = -1;
-					}else {
-						list.remove(cursor);
-					}
+				if(iterator.hasPrevious()) {
+					iterator.previous();
+					iterator.remove();
 				}
-				
-				// 커서가 맨 오른쪽에 있을 때
-				if(cursor == list.size()) {
-					cursor--;
-					list.remove(cursor);
-				}
-				
-				// 커서가 맨 왼쪽에 있을 때
-				if(cursor == -1) {
-					cursor = -1;
-				}
+				continue;
 			}
 			
 			// 커서 위치에 값 추가
 			if(operator == 'P') {
-				value = command.charAt(2);
+				value = command.charAt(2);		
 				
-				// 커서가 중간에 있을 때
-				if(cursor != list.size() && cursor != -1) {
-					list.add(cursor, value);
-					cursor++;					
-				}
-				
-				// 커서가 맨 오른쪽에 있을 때
-				if(cursor == list.size()) {
-					list.add(cursor, value);
-					cursor++;
-				}
-				
-				// 커서가 맨 왼쪽에 있을 때
-				if(cursor == -1) {
-					cursor++;
-					list.add(cursor, value);
-				}
+				iterator.add(value);
+				continue;
 			}
-			
-			System.out.println();
-			System.out.println("커서: " + cursor);
-			System.out.println("반복 횟수: " + (i + 1) + "	" + list);
-			System.out.println("-------------------------------");
 		}
 		
 		for(char c : list) {
 			sb.append(c);
 		}
 		
-		System.out.println(sb.toString());
+		// sb.toString은 문자열 길이에 비례하다.
+		System.out.println(sb);
 		br.close();
 	}
 }

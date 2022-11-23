ac5b07838e218c577a92306ae65f3f61df6e1330
sange17 <sange17@naver.com>
2022-11-23 09:40:21 +0900

    Add implementation_11719.java

---
+++ src/Implementation/Implementation_11719.java
@@ -0,0 +1,19 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class Implementation_11719 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		String str;
+		while(sc.hasNextLine()) {
+			
+			str = sc.nextLine();
+			System.out.println(str);
+		}
+		
+		sc.close();
+	}
+}

f65ce788da8d71de3a0f260588a23e9ac1447c22
sange17 <sange17@naver.com>
2022-12-30 20:25:02 +0900

    Add implementation_15000.java

---
+++ src/Implementation/Implementation_15000.java
@@ -0,0 +1,13 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class Implementation_15000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str.toUpperCase());
+	}
+}

0dfa8f89e9141e733ec830143657226d4b46bfb3
sange17 <sange17@naver.com>
2023-01-01 21:10:42 +0900

    Add implementation_21598.java

---
+++ src/Implementation/Implementation_21598.java
@@ -0,0 +1,20 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class Implementation_21598 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		long N = sc.nextLong();
+		
+		for(long i = 0; i < N; i++) {
+			
+			sb.append("SciComLove\n");
+		}
+		
+		System.out.println(sb);
+		sc.close();
+	}
+}

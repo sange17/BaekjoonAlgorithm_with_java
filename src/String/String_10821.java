9085a9aeb424147beed9317453353e1b60919961
sange17 <sange17@naver.com>
2023-02-02 21:56:49 +0900

    Add string_10821.java

---
+++ src/String/String_10821.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class String_10821 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] array = sc.nextLine().split(",");
+		
+		System.out.println(array.length);
+	}
+}
